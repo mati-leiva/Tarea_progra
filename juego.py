446ed66d631e80d4648a68c072290f77b84ba860
Matias Leiva <m.leiva05@ufromail.cl>
2024-10-27 18:02:19 -0300

    better movement

--- juego.py
+++ juego.py
@@ -11,23 +11,23 @@ class Player:
         self.pos_y = pos_y
         self.vel_x = 2.
         self.vel_y = 2.
+        self.rect = (self.pos_x,self.pos_y,20,20)
         
     def draw_point(self):
         pygame.draw.rect(DISPLAYSURF, RED, (self.pos_x ,self.pos_y,20,20))
     
     def movement(self):
         if pygame.key.get_pressed()[pl.K_a]:
-            self.vel_x -= 0.1
+            self.vel_x -= 0.2
         if pygame.key.get_pressed()[pl.K_d]:
-            self.vel_x += 0.1
+            self.vel_x += 0.2
         if pygame.key.get_pressed()[pl.K_w]:
-            self.vel_y -= 0.1
+            self.vel_y -= 0.2
         if pygame.key.get_pressed()[pl.K_s]:
-            self.vel_y += 0.1     
+            self.vel_y += 0.2     
             
         self.velocity_limit()
         self.update_position() 
-        
         
     def velocity_limit(self):
         if (np.array([self.vel_x,self.vel_y])**2).sum()**0.5>=4.:
@@ -35,13 +35,24 @@ class Player:
             self.vel_y = self.vel_y * 0.5 
         
     def update_position(self):
-        self.pos_x += self.vel_x
-        self.pos_y += self.vel_y
-
         
+        if 8 < self.pos_x < width - 8:
+            self.pos_x += self.vel_x
+        elif self.pos_x <= 11:
+            self.vel_x = 0
+            self.pos_x = 10
+        elif self.pos_x >= width - 11:
+            self.vel_x = 0
+            self.pos_x = width - 10
 
-
-
+        if 8 < self.pos_y < height - 8:
+            self.pos_y += self.vel_y
+        elif self.pos_y <= 11:
+            self.vel_y = 0
+            self.pos_y = 9
+        elif self.pos_y >= height - 11:
+            self.vel_y = 0
+            self.pos_y = height - 10
 
 class Point:
     def __init__(self, pos_x, pos_y):
@@ -51,7 +62,7 @@ class Point:
         self.vel_y = 0.
       
     def draw_point(self):
-        pygame.draw.rect(DISPLAYSURF, GREEN, (self.pos_x ,self.pos_y,20,20))
+        pygame.draw.rect(DISPLAYSURF, GREEN, (self.pos_x ,self.pos_y,20,20))    
 
     def direction_to_char(self,x,y):
             
@@ -68,40 +79,38 @@ class Point:
         
         Point.update_position(self)
 
-    
     def velocity_limit(self):
-        if (np.array([self.vel_x,self.vel_y])**2).sum()**0.5>=1.5:
+        if (np.array([self.vel_x,self.vel_y])**2).sum()**0.5>=2.5:
             self.vel_x = self.vel_x * 0.5
             self.vel_y = self.vel_y * 0.5 
 
     def update_position(self):
         self.pos_x += self.vel_x
         self.pos_y += self.vel_y
+        pass
+
 
 def display_game():
     # We display a colection of points
     Point_1 = Point(200,200)
-    Point_2 = Point(190,190)
     Player_1 = Player(100,100)
     
     while True:
-        
         for event in pygame.event.get():
-            
             if event.type == pl.QUIT:
-                pygame.quit()
-                sys.exit()
-                
+                    pygame.quit()
+                    sys.exit()
+                    
         DISPLAYSURF.fill(WHITE)   
         Point_1.draw_point()
         Player_1.draw_point()
-        
+            
         Player_1.movement()
         Point_1.direction_to_char(Player_1.pos_x,Player_1.pos_y)
-        
+            
         pygame.display.update()
         clock.tick(60)
-        
+            
 # Starting the colors
 BLACK = (10,   10,   10)
 WHITE = (240, 240, 240)
@@ -110,7 +119,10 @@ GREEN = (31, 255,   10)
 BLUE = (10,   10, 255)
 
 # Seting up the display
-DISPLAYSURF = pygame.display.set_mode((400, 400), 0, 32)
+width, height = 400, 400
+
+
+DISPLAYSURF = pygame.display.set_mode((width,height),0, 32)
 clock = pygame.time.Clock()  
 pygame.display.set_caption("hello World")
 # Starting the game
